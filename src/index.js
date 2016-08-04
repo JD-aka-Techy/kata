b0a2b026358e6ba2405288fdcad16507ada1104c
JD-aka-Techy <jrdavenport85@gmail.com>
2016-08-04 12:23:49 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+require('./styles/style.sass') // root stylesheeet - .css, .scss or .sass
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import App from './components/app';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
